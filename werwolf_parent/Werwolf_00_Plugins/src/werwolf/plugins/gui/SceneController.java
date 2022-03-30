961aaaf14b53d3ad2846eb66fd145c5765342a47
Luna Zacharias <72688158+SeraLunatic@users.noreply.github.com>
2022-03-30 22:20:46 +0200

    werwolfpixel hinzugefügt

--- werwolf_parent/Werwolf_00_Plugins/src/werwolf/plugins/gui/SceneController.java
+++ werwolf_parent/Werwolf_00_Plugins/src/werwolf/plugins/gui/SceneController.java
@@ -14,13 +14,19 @@ public class SceneController {
 	private Stage stage;
 	private Scene scene;
 	
+	String css;
+	
 	
 	public void play(ActionEvent e) throws IOException {
 		//TODO Play Button
 		System.out.println("Play");
 		root = FXMLLoader.load(getClass().getResource("PlayMain.fxml"));
+		css = getClass().getResource("application.css").toExternalForm();
+		
 		stage = (Stage)((Node)e.getSource()).getScene().getWindow();
 		scene = new Scene(root);
+		scene.getStylesheets().add(css);
+		
 		stage.setScene(scene);
 		stage.show();
 		
